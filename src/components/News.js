5ff88bd3a5fa6c5eb661d7989875da1008bc51d8
Dharmik <20it087@charusat.edu.in>
2023-05-28 15:58:44 +0530

    Fetching News category wise in Daily-Top-News React App

--- src/components/News.js
+++ src/components/News.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
-import Spinner from './Spinner'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+    static defaultProps = {
+        country: 'in',
+        pageSize: 8,
+        category: 'general',
+    }
+
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string,
+    }
 
     constructor() {
-        super()
-        console.log("this is contructor from news ")
+        super();
         this.state = {
             articles: [],
             loading: false,
@@ -15,12 +27,11 @@ export class News extends Component {
     }
 
     async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b173ce3376c43be820c84de66cd6e30&page=1&pagesize=${this.props.pageSize}`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2b173ce3376c43be820c84de66cd6e30&page=1&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
-        // now fwtch the data
         let data = await fetch(url);
-        // parse the data 
         let parsedData = await data.json()
+        console.log(parsedData);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
@@ -28,66 +39,53 @@ export class News extends Component {
         })
     }
 
-    handlePreclick = async () => {
-
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b173ce3376c43be820c84de66cd6e30&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
+    handlePrevClick = async () => {
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2b173ce3376c43be820c84de66cd6e30&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
-        // now fwtch the data
         let data = await fetch(url);
-        // parse the data 
         let parsedData = await data.json()
-
-        // in next button update the page 
+        console.log(parsedData);
         this.setState({
             page: this.state.page - 1,
             articles: parsedData.articles,
             loading: false
-
-
         })
-
 
     }
 
-    handleNextclick = async () => {
-
+    handleNextClick = async () => {
+        console.log("Next");
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2b173ce3376c43be820c84de66cd6e30&page=${this.state.page + 1}&pageSize=${this.props.pageSize} `
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2b173ce3376c43be820c84de66cd6e30&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             this.setState({ loading: true });
-            // now fwtch the data
             let data = await fetch(url);
-            // parse the data 
             let parsedData = await data.json()
-            // in next button update the page 
             this.setState({
                 page: this.state.page + 1,
                 articles: parsedData.articles,
                 loading: false
-
             })
-
         }
     }
+
     render() {
         return (
             <div className="container my-3">
-                <h2 className="text-center">Daily-Top-News - Top Headlines</h2>
-                {this.state.loading && < Spinner />}
+                <h1 className="text-center" style={{ margin: '35px 0px' }}>Daily-Top-News App - Top Headlines</h1>
+                {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
-
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title.slice(0, 40) : ""} description={element.description ? element.description.slice(0, 85) : ""} imgurl={element.urlToImage} newsurl={element.url} />
-
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                     })}
                 </div>
-                <div className="container d-flex justify-content-between" >
-                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreclick}>&larr;Previous</button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextclick}>Next &rarr;</button>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
-            </div >
+            </div>
         )
     }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,21 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        // Creating props for tital , description ...
-        let { title, description, imgurl, newsurl } = this.props;
+        let { title, description, imageUrl, newsUrl } = this.props;
         return (
-            <div className="my-3" >
-
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src={!imgurl ? "https://imgeng.jagran.com/images/2023/may/fake-chatgpt-apps1684648974136.jpg" : imgurl} className="card-img-top" alt="..." />
+            <div className="my-3">
+                <div className="card">
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
-                        <a href={newsurl} target='_blank' rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
-
         )
     }
 }
